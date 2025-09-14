dfd279c70a1d2e6b9a547791f88a6331b842e3b3
Karthik <karthikk@kitaab.biz>
2025-09-14 19:07:47 +0530

    fix: bugs resolved

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -14,7 +14,7 @@ function NavBar() {
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl" onClick={() => navigate('/feed')}>Dev Tinder</a>
+        <a className="btn btn-ghost text-xl" onClick={() => navigate('/feed')}>Dev Tinderr</a>
       </div>
       <div className="flex gap-2">
         <div className="dropdown dropdown-end mx-5">
